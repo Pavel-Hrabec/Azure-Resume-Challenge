28275ea1a487486184f07e56cf554fae36fe6599
Pavel-Hrabec <hrabecpavel@gmail.com>
2022-02-28 16:21:27 +0100

    mainjs and readme edited

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Azure-Resume-Challenge
 Azure-Resume
+
+## The Beginning
+
+- Edited index.html
+- main.js contains counter and description of the code
--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,20 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', (event) =>{ // Everytime content is loaded go and run the getVisitCount 
     getVisitCount();
 })
 
-const functionApiUrl = 'https://getresumecounter.azurewebsites.net/api/GetResumeCounter?code=Q/LkPt0mhQKdP8DoE3DdllAYdFQO//58Iq7AoS6JUBj9FgJ86Rqt8A==';
-const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = '';
+// const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl).then(response => {
-        return response.json()
-    }).then(response =>{
-        console.log("Website called function API.");
-        count =  response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
-        console.log(error);
+    fetch(functionApiUrl).then(response => { // fetch will get data supplied by API fuction, once you do that, grab the response and 
+        return response.json() // return it json
+    }).then(response =>{ // grab the response in json 
+        console.log("Website called function API."); //log for debugging, just in case
+        count =  response.count; // we will count value in rensponse
+        document.getElementById("counter").innerText = count; // Go into the document, find the element that has counter as it's id and set the value to count 
+    }).catch(function(error){ // if there is an error we will grab it and 
+        console.log(error); // display it in console
     });
-    return count;
+    return count; // 
 }
