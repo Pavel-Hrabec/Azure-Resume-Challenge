1dd0352439d7e4e9037f37f3f26afb0e8b5bed5a
Pavel-Hrabec <hrabecpavel@gmail.com>
2022-03-19 14:26:01 +0100

    trying to fix PythonCounter

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -5,6 +5,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{ // Everytime content is 
 const functionApiUrl = 'https://getazresumecounter.azurewebsites.net/api/GetResCounter?code=jbHcOLlHPZaNXqe2IBaOogMvGVhTrH8QuvRHMwEv1dtg3vvue9PU8A=='
 const localfunctionApiUrl = 'http://localhost:7071/api/GetResCounter'
 const PythonApiUrl = 'https://pythoncounter.azurewebsites.net/api/PythonCounter?code=jVHT95QIzC08iLyqaV9s6OLiCs2LHxqhHJkkp8YGZNuoUYAYnHpRsw=='
+const LocalPythonApiUrl = 'http://localhost:7071/api/PythonCounter'
 
 const getVisitCount = () => {
     let count = 30;
@@ -12,7 +13,7 @@ const getVisitCount = () => {
         return response.json() // return it json
     }).then(response =>{ // grab the response in json 
         console.log("Website called function API."); //log for debugging, just in case
-        count =  response.count; // we will count value in rensponse
+        count =  response.count; // we will count value in response
         document.getElementById("counter").innerText = count; // Go into the document, find the element that has counter as it's id and set the value to count 
     }).catch(function(error){ // if there is an error we will grab it and 
         console.log(error); // display it in console
